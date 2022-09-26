601a9fae54415a64e4482f69e0bf2e7598cef9ad
Karol1331 <91654319+Karol1331@users.noreply.github.com>
2022-09-26 10:22:36 +0200

    Add files via upload

---
+++ egzamin/fb/lista.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styl.css">
+    <title>Lista przyjaciół</title>
+</head>
+<body>
+    <header>
+        <section id="baner"><h1>Portal Społecznościowy - moje konto</h1></section>
+    </header>
+    <main>
+        <section id="glowny">
+            <h2>Moje zainteresowania</h2>
+            <ul>
+                <li>muzyka</li>
+                <li>film</li>
+                <li>komputery</li>
+            </ul>
+            <h2>Moi znajomi</h2>
+            <!-- skrypt -->
+        </section>
+    </main>
+    <footer>
+        <section id="lewy">
+            Stronę wykonał: 00000000000
+        </section>
+        <section id="prawy">
+            <a href="mailto:ja@portal.pl">napisz do mnie</a>
+        </section>
+    </footer>
+</body>
+</html>
