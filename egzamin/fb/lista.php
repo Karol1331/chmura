688b2668736b72d0dd12e06ff60bc8ba7980600d
Karol1331 <91654319+Karol1331@users.noreply.github.com>
2022-09-26 15:25:29 +0200

    Update lista.php

--- egzamin/fb/lista.php
+++ egzamin/fb/lista.php
@@ -20,8 +20,27 @@
                 <li>komputery</li>
             </ul>
             <h2>Moi znajomi</h2>
-            <!-- skrypt -->
+            <?php
+            $conn = new mysqli("localhost", "root", "", "social");
+            if(!$conn) die("Wystąpił błąd!");
+
+            $sql = "SELECT imie, nazwisko, opis, zdjecie FROM osoby WHERE Hobby_id = 1 OR Hobby_id = 2 OR Hobby_id = 6";
+            if($result = $conn->query($sql)) {
+                foreach($result as $wiersz) {
+                    $fot = $wiersz['zdjecie'];
+
+                    $imie = $wiersz['imie'];
+                    $nazwisko = $wiersz['nazwisko'];
+                    $opis = $wiersz['opis'];
+                    echo "<div><div class='zdjecie'><img src='$fot' alt='przyjaciel'></div>
+                    <div class='opis'><h3>$imie $nazwisko</h3><p>Ostatni wpis: $opis</p></div><br>
+                    <div class='linia'><hr></div></div>";
+                }
+            }
+            $conn->close();
+            ?>
         </section>
+        
     </main>
     <footer>
         <section id="lewy">
